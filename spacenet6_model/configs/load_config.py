c581a2c450193df6ac9cc2e1f1a7e34649db8931
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-03-16 13:02:39 +0900

    refactor with get_config_with_previous_experiment()

--- spacenet6_model/configs/load_config.py
+++ spacenet6_model/configs/load_config.py
@@ -5,15 +5,15 @@ from .defaults import get_default_config
 from ..utils import config_filename, experiment_subdir, weight_best_filename
 
 
-def _load_previous_experiment(config, exp_id, log_dir):
+def _load_previous_experiment(config, exp_id, exp_log_dir):
     """
     """
-    log_dir = log_dir if log_dir else config.LOG_ROOT
+    exp_log_dir = exp_log_dir if exp_log_dir else config.LOG_ROOT
     exp_subdir = experiment_subdir(exp_id)
 
     # overwrite config
     config_path = os.path.join(
-        log_dir,
+        exp_log_dir,
         exp_subdir,
         config_filename()
     )
@@ -27,6 +27,21 @@ def _load_previous_experiment(config, exp_id, log_dir):
     )
     config.MODEL.WEIGHT = weight_path
 
+    return config
+
+
+def get_config_with_previous_experiment(exp_id=-1, exp_log_dir=None):
+    """
+    """
+    config = get_default_config()
+    if exp_id >= 0:
+        # overwrite hyper parameters with the ones used by previous experiment
+        config = _load_previous_experiment(
+            config, exp_id, exp_log_dir
+        )
+    else:
+        # if exp_id < 0, return default config
+        pass
     return config
 
 
@@ -58,18 +73,16 @@ def load_config():
     )
     args = parser.parse_args()
 
-    config = get_default_config()
-    if args.exp_id >= 0:
-        config = _load_previous_experiment(
-            config,
-            args.exp_id,
-            args.exp_log_dir
-        )
+    config = get_config_with_previous_experiment(
+        exp_id=args.exp_id,
+        exp_log_dir=args.exp_log_dir
+    )
     if args.config:
+        # overwrite hyper parameters with the ones given by the YAML file.
         config.merge_from_file(args.config)
     if args.opts:
+        # overwrite hyper parameters with the ones given by command line args.
         config.merge_from_list(args.opts)
-
     config.freeze()
 
     return config

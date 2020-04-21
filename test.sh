a89cdad21c6bbb9a00c9f59dacb9eb719d37c757
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-04-21 15:16:32 +0900

    use absolute paths in test.sh

--- test.sh
+++ test.sh
@@ -7,13 +7,13 @@ source activate solaris
 TEST_DIR=$1  # path/to/spacenet6/test/AOI_11_Rotterdam/
 OUTPUT_CSV_PATH=$2  # path/to/solution.csv
 
-source settings.sh
+source /work/settings.sh
 
 # predict with trained models
-./scripts/test/test_cnns.sh ${TEST_DIR}
+./work/scripts/test/test_cnns.sh ${TEST_DIR}
 
 # postprocess predictions
-./scripts/test/postprocess.sh ${TEST_DIR} ${OUTPUT_CSV_PATH}
+./work/scripts/test/postprocess.sh ${TEST_DIR} ${OUTPUT_CSV_PATH}
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo 'Total time for testing: ' $(($ELAPSED_TIME/60)) '[min]'

daf3c71301430c5ea5288072f760354940c3ca6f
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-04-29 15:08:39 +0900

    bug fix

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ START_TIME=$SECONDS
 
 source activate solaris
 
-rm -rf ${MODEL_PREDICTION_DIR} ${ENSEMBLED_PREDICTION_DIR} ${POLY_CSV_DIR}
-
 TEST_DIR=$1  # path/to/spacenet6/test/AOI_11_Rotterdam/
 OUTPUT_CSV_PATH=$2  # path/to/solution.csv
 
 source /work/settings.sh
+
+rm -rf ${MODEL_PREDICTION_DIR} ${ENSEMBLED_PREDICTION_DIR} ${POLY_CSV_DIR}
 
 # predict with trained models
 /work/scripts/test/test_cnns.sh ${TEST_DIR}

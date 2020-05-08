472b992cfae5ea2b0af123660246e8843559cf1b
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-05-08 11:55:30 +0900

    disable nohup

--- scripts/test/test_cnns.sh
+++ scripts/test/test_cnns.sh
@@ -25,117 +25,72 @@ echo ''
 echo 'predicting... (1/4)'
 
 ## CONFIG_A_2
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 5 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0005.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 6 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0006.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 7 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0007.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 8 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0008.out 2>&1 &
-
-wait
+    ${TEST_ARGS}
 
 echo 'predicting... (2/4)'
 
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 9 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0009.out 2>&1 &
-
-wait
+    ${TEST_ARGS}
 
 ## CONFIG_B_1
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 10 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0010.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 11 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0011.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 12 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0012.out 2>&1 &
-
-wait
+    ${TEST_ARGS}
 
 echo 'predicting... (3/4)'
 
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 13 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0013.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 14 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0014.out 2>&1 &
-
-wait
+    ${TEST_ARGS}
 
 ## CONFIG_C_1
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 15 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0015.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 16 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0016.out 2>&1 &
-
-wait
+    ${TEST_ARGS}
 
 echo 'predicting... (4/4)'
 
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 17 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0017.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 18 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0018.out 2>&1 &
+    ${TEST_ARGS}
 
-wait
-
-nohup env CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
+CUDA_VISIBLE_DEVICES=0 /work/tools/test_spacenet6_model.py \
     --exp_id 19 \
-    ${TEST_ARGS} \
-    > ${TEST_STDOUT_DIR}/exp_0019.out 2>&1 &
-
-wait
+    ${TEST_ARGS}
 
 echo 'done predicting all models!'

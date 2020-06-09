8af1ed55facfeeef28a280017b664e9ce8580645
Motoki Kimura <motoki.kimura.1990@gmail.com>
2020-06-09 14:47:23 +0900

    fix ssl error

--- tools/train_spacenet6_model.py
+++ tools/train_spacenet6_model.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 import os
-import segmentation_models_pytorch as smp
+import ssl
 import timeit
-import torch
 
+import torch
 from tensorboardX import SummaryWriter
 
+import segmentation_models_pytorch as smp
 from _init_path import init_path
-init_path()
-
 from spacenet6_model.configs import load_config
 from spacenet6_model.datasets import get_dataloader
 from spacenet6_model.evaluations import get_metrics
 from spacenet6_model.models import get_model
-from spacenet6_model.solvers import (
-    get_loss, get_lr_scheduler, get_optimizer
-)
+from spacenet6_model.solvers import get_loss, get_lr_scheduler, get_optimizer
 from spacenet6_model.utils import (
-    checkpoint_epoch_filename, checkpoint_latest_filename,
-    config_filename, dump_git_info, experiment_subdir, git_filename,
-    load_latest_checkpoint, save_checkpoint,
-    weight_best_filename
+    checkpoint_epoch_filename,
+    checkpoint_latest_filename,
+    config_filename,
+    dump_git_info,
+    experiment_subdir,
+    git_filename,
+    load_latest_checkpoint,
+    save_checkpoint,
+    weight_best_filename,
 )
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+init_path()
 
 
 def main():
@@ -29,9 +35,9 @@ def main():
     """
 
     config = load_config()
-    print('successfully loaded config:')
+    print("successfully loaded config:")
     print(config)
-    print('')
+    print("")
 
     epoch_to_start_val = config.EVAL.EPOCH_TO_START_VAL
     assert config.SOLVER.EPOCHS > epoch_to_start_val
@@ -63,14 +69,8 @@ def main():
     best_score = 0
 
     # load checkpoint if exists
-    model, optimizer, lr_scheduler, start_epoch, best_score = \
-    load_latest_checkpoint(
-        checkpoint_dir,
-        model,
-        optimizer,
-        lr_scheduler,
-        start_epoch,
-        best_score
+    model, optimizer, lr_scheduler, start_epoch, best_score = load_latest_checkpoint(
+        checkpoint_dir, model, optimizer, lr_scheduler, start_epoch, best_score
     )
 
     # prepare metrics and loss
@@ -87,11 +87,7 @@ def main():
         verbose=True,
     )
     val_epoch = smp.utils.train.ValidEpoch(
-        model,
-        loss=loss,
-        metrics=metrics,
-        device=config.MODEL.DEVICE,
-        verbose=True,
+        model, loss=loss, metrics=metrics, device=config.MODEL.DEVICE, verbose=True,
     )
 
     # prepare tensorboard
@@ -102,7 +98,7 @@ def main():
         dump_git_info(os.path.join(log_dir, git_filename()))
 
     # dump config to a file
-    with open(os.path.join(log_dir, config_filename()), 'w') as f:
+    with open(os.path.join(log_dir, config_filename()), "w") as f:
         f.write(str(config))
 
     # train loop
@@ -110,17 +106,17 @@ def main():
     split_id = config.INPUT.TRAIN_VAL_SPLIT_ID
 
     for epoch in range(start_epoch, config.SOLVER.EPOCHS):
-        lr = optimizer.param_groups[0]['lr']
-        print(f'\nEpoch: {epoch}, lr: {lr}')
+        lr = optimizer.param_groups[0]["lr"]
+        print(f"\nEpoch: {epoch}, lr: {lr}")
 
         # run train for 1 epoch
         train_logs = train_epoch.run(train_dataloader)
 
         # log lr to tensorboard
-        tblogger.add_scalar('lr', lr, epoch)
+        tblogger.add_scalar("lr", lr, epoch)
         # log train losses and scores
         for k, v in train_logs.items():
-            tblogger.add_scalar(f'split_{split_id}/train/{k}', v, epoch)
+            tblogger.add_scalar(f"split_{split_id}/train/{k}", v, epoch)
 
         if epoch >= config.EVAL.EPOCH_TO_START_VAL:
             # run val for 1 epoch
@@ -128,18 +124,17 @@ def main():
 
             # log val losses and scores
             for k, v in val_logs.items():
-                tblogger.add_scalar(f'split_{split_id}/val/{k}', v, epoch)
+                tblogger.add_scalar(f"split_{split_id}/val/{k}", v, epoch)
 
             # save model weight if score updated
             if best_score < val_logs[metric_name]:
                 best_score = val_logs[metric_name]
                 torch.save(
-                    model.state_dict(),
-                    os.path.join(weight_dir, weight_best_filename())
+                    model.state_dict(), os.path.join(weight_dir, weight_best_filename())
                 )
-                print('Best val score updated!')
+                print("Best val score updated!")
         else:
-            print(f'Skip val until epoch {epoch_to_start_val}')
+            print(f"Skip val until epoch {epoch_to_start_val}")
 
         # update lr for the next epoch
         lr_scheduler.step()
@@ -148,18 +143,28 @@ def main():
             # save checkpoint every epoch
             save_checkpoint(
                 os.path.join(checkpoint_dir, checkpoint_epoch_filename(epoch)),
-                model, optimizer, lr_scheduler, epoch + 1, best_score)
+                model,
+                optimizer,
+                lr_scheduler,
+                epoch + 1,
+                best_score,
+            )
             save_checkpoint(
                 os.path.join(checkpoint_dir, checkpoint_latest_filename()),
-                model, optimizer, lr_scheduler, epoch + 1, best_score)
+                model,
+                optimizer,
+                lr_scheduler,
+                epoch + 1,
+                best_score,
+            )
 
     tblogger.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     t0 = timeit.default_timer()
 
     main()
 
     elapsed = timeit.default_timer() - t0
-    print('Time: {:.3f} min'.format(elapsed / 60.0))
+    print("Time: {:.3f} min".format(elapsed / 60.0))
